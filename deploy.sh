873ddce1734e583f0ae15462ab19a3edd1d7f110
akashktr27 <akashkantrikar@gmail.com>
2024-02-13 15:41:38 +0530

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 SSH_USER="ubuntu"
 SSH_HOST="52.35.169.89"
-SSH_KEY="/home/ubuntu/sacumen-keypair.pem"
+SSH_KEY="/home/ubuntu/01-login/ssh/sacumen-keypair.pem"
 
 # SSH into the EC2 instance and copy your code changes
 ssh -i "$SSH_KEY" "$SSH_USER"@"$SSH_HOST" <<EOF
