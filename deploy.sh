1eb8e60611efbd8966cb0d5b81db3f00c769b360
akashktr27 <akashkantrikar@gmail.com>
2024-02-14 00:23:36 +0530

    final testing

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ USERNAME="ubuntu"
 INSTANCE_IP="52.35.169.89"
 REPO_DIRECTORY="/home/ubuntu/01-login/ssh"
 ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_PATH" "$USERNAME@$INSTANCE_IP"<< EOF
-   cd /home/ubuntu/01-login && git reset --hard origin/master
+   cd /home/ubuntu/01-login && git reset --hard origin/master %% git pull
 EOF
