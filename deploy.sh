723033766944ed10f5c241e229f7dabdbccc80a9
akashktr27 <akashkantrikar@gmail.com>
2024-02-14 00:06:53 +0530

    add ip lasr

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# Set the path to your private key file
 PRIVATE_KEY_PATH="ssh/sacumen-keypair.pem"
-
-# Set the username to connect with (default is "ec2-user" for Amazon Linux instances)
 USERNAME="ubuntu"
-
-# Set the public IP address or DNS name of your EC2 instance
 INSTANCE_IP="52.35.169.89"
-
-
-# Set the path to the directory where your git repository is located on the EC2 instance
 REPO_DIRECTORY="/home/ubuntu/01-login/ssh"
-
-# SSH connection command to perform a git pull
-ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_PATH" "$USERNAME@$INSTANCE_IP"
-<< EOF
-  cd REPO_DIRECTORY
-  chmod +x git_pull.sh
-  ./git_pull.sh
-  exit
+ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_PATH" "$USERNAME@$INSTANCE_IP"<< EOF
+   'echo "This is a test file" > ~/test_file1.txt'
 EOF
