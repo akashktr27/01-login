f9254c48574d2dfef02d6bb0c6aed63bf143afd7
akashktr27 <akashkantrikar@gmail.com>
2024-02-14 00:10:23 +0530

    add ip lasr

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ USERNAME="ubuntu"
 INSTANCE_IP="52.35.169.89"
 REPO_DIRECTORY="/home/ubuntu/01-login/ssh"
 ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_PATH" "$USERNAME@$INSTANCE_IP"<< EOF
-   'echo "This is a test file" > ~/test_file1.txt'
+   touch sample.txt
 EOF
