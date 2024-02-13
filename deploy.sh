47d63e9e5d3e18ca430738b9b305fed2c65e435c
akashktr27 <akashkantrikar@gmail.com>
2024-02-13 18:34:06 +0530

    final last test

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SSH_KEY="/home/ubuntu/01-login/ssh/sacumen-keypair.pem"
 # SSH into the EC2 instance and copy your code changes
 ssh -i "$SSH_KEY" "$SSH_USER"@"$SSH_HOST" <<EOF
   cd /home/ubuntu/01-login
-  sudo apt update
+  sudo apt updatec
   sudo apt install python3-pip -y
   git pull origin master
   pip install -r requirements.txt
