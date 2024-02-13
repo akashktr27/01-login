520ee56600c1c629dbdfbde65066adac415a61b9
akashktr27 <akashkantrikar@gmail.com>
2024-02-13 15:40:43 +0530

    add pem

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ SSH_KEY="/home/ubuntu/sacumen-keypair.pem"
 
 # SSH into the EC2 instance and copy your code changes
 ssh -i "$SSH_KEY" "$SSH_USER"@"$SSH_HOST" <<EOF
-  cd /path/to/your/project
+  cd /home/ubuntu/01-login
   git pull origin master
+  python3 manage.py runserver
   # Restart your application if needed
 EOF
