8b8340038f4a1892c478388c3c2c58705ee818bf
akashktr27 <akashkantrikar@gmail.com>
2024-02-15 12:47:19 +0530

    update setings

--- webappexample/settings.py
+++ webappexample/settings.py
@@ -108,9 +108,5 @@ if ENV_FILE:
 # Load Auth0 application settings into memory
 
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
-print('domain', AUTH0_DOMAIN)
 AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
-
-
-print("test check in build")
